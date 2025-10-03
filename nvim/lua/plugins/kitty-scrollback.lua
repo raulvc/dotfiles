d1a2b2b34a5952357b885e7a29289277e1731acd
Raul Cioldin <raulvc@gmail.com>
2025-10-03 16:32:39 -0300

    feat(kitty): integrate kitty-scrollback.nvim

---
+++ nvim/lua/plugins/kitty-scrollback.lua
@@ -0,0 +1,43 @@
+return {
+  "mikesmithgh/kitty-scrollback.nvim",
+  enabled = true,
+  lazy = true,
+  cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+  event = { "User KittyScrollbackLaunch" },
+  config = function()
+    require("kitty-scrollback").setup {
+      {
+        -- Default configuration for all windows
+        paste_window = {
+          yank_register_enabled = true,
+          yank_register = "+",
+        },
+        status_window = {
+          enabled = true,
+          style_simple = false,
+        },
+        keymaps_enabled = true,
+        restore_options = true,
+        highlight_overrides = {
+          KittyScrollbackNvimStatusWinNormal = { link = "Normal" },
+          KittyScrollbackNvimPasteWinNormal = { link = "Normal" },
+        },
+      },
+      -- Configuration for fzf search
+      ksb_builtin_last_cmd_output = function()
+        return {
+          paste_window = {
+            yank_register_enabled = false,
+          },
+          callbacks = {
+            on_ready = function()
+              vim.cmd "normal! G"
+              vim.cmd "normal! ?\\$ <CR>" -- Find last prompt
+              vim.cmd "FzfLua lines"
+            end,
+          },
+        }
+      end,
+    }
+  end,
+}
