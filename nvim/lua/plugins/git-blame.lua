d864b95d7091df571f8f3adb07d91eab17bdfded
Raul Cioldin <raulvc@gmail.com>
2026-07-10 16:30:11 -0300

    refactor(git): remove lazygit plugin configuration

--- nvim/lua/plugins/git-blame.lua
+++ nvim/lua/plugins/git-blame.lua
@@ -1,29 +1,5 @@
 return {
   {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-    config = function()
-      require("telescope").load_extension "lazygit"
-    end,
-  },
-  {
     "f-person/git-blame.nvim",
     -- load the plugin at startup
     event = "VeryLazy",
